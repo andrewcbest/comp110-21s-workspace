9bfe68f09bbb88a3b709bcb9a0bce0b8b444eb26
Andrew Best <acandrew@email.unc.edu>
2021-02-06 16:37:47 -0500

    finished exercise 2

--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -1,6 +1,20 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730390102"
 
 
 # Begin your solution here...
+number: str = input("Enter an int:")
+
+num = int(number)
+
+if num % 14 == 0: 
+    print("TAR HEELS")
+else:
+    if num % 7 == 0: 
+        print("HEELS")
+    else:
+        if num % 2 == 0:
+            print("TAR")
+        else:
+            print("CAROLINA")
