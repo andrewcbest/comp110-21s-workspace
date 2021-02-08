65a2f6d44c65728785d122699e1bdbe7f164555a
Andrew Best <acandrew@email.unc.edu>
2021-02-08 15:53:25 -0500

    Completed exercise 3

--- exercises/ex01/vaccine_calc.py
+++ exercises/ex01/vaccine_calc.py
@@ -1,6 +1,6 @@
 """A vaccination calculator."""
 
-__author__ = "YOUR PID HERE"
+__author__ = "730390102"
 
 # The datetime data type is imported from the datetime library.
 # A datetime object models a specific date and time.
@@ -18,3 +18,27 @@ from datetime import timedelta
 
 
 # Begin your solution here...
+population_input = input("Enter population.")
+population = float(population_input)
+
+doses_admined_input = input("Enter the number of vaccine doses already given.")
+doses_admined = float(doses_admined_input)
+
+doses_per_day_input = input("Enter the number of vaccine doses given per day moving forward.")
+doses_per_day = float(doses_per_day_input)
+
+target_percent_input = input("Enter the target percent of the population to be vaccinated.")
+target_percent = float(target_percent_input)
+
+target_population: float = population * (target_percent/100)
+target_doses: float = target_population * 2
+doses_left: float = target_doses - doses_admined
+days_left: float = (doses_left / doses_per_day)
+today: datetime = datetime.today()
+days_left_time: timedelta = timedelta(days_left)
+date_complete: datetime = today + days_left_time
+
+target_percent_output = str(int(round(target_percent)))
+days_left_output = str(int(float(days_left)))
+
+print("We will reach " + target_percent_output + "% vaccination in " + days_left_output + " days, which falls on " + date_complete.strftime("%B %d, %Y") + ".") 
