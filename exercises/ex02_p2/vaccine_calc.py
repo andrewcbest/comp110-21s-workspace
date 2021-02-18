eca1d6b27ded4df8553819fb59de2ce5d7645d7c
Andrew Best <acandrew@email.unc.edu>
2021-02-18 16:45:39 -0500

    corrected vaccine_calc

--- exercises/ex02_p2/vaccine_calc.py
+++ exercises/ex02_p2/vaccine_calc.py
@@ -12,7 +12,7 @@ def main() -> None:
     doses_per_day: int = int(input("Doses per day: "))
     target: int = int(input("Target percent vaccinated: "))
     # TODO 2: Call days_to_target and store result in a variable.
-    days_left = days_to_target(population, doses, doses_per_day, target)
+    days_left: int = days_to_target(population, doses, doses_per_day, target)
     # TODO 4: Call future_date and store result in a variable.
     date_complete = future_date(days_left)
     # TODO 5: Print the expected output using the variables above.
@@ -20,18 +20,20 @@ def main() -> None:
     print(days_left)
     print(" days, which falls on ")
     print(date_complete)
+
+    
 # TODO 1: Define days_to_target function
-def days_to_target(population, doses, doses_per_day, target) -> int:
-    """Determines the number of days to meet vaccination target"""
-    doses_left: int = doses_per_day * (target/100)
-    doses_left: int = population * 2
-    doses_left = doses_left - doses
-    return(doses_left / doses_per_day)
+def days_to_target(population: int, doses: int, doses_per_day: int, target: int) -> int:
+    """Determines the number of days to meet vaccination target."""
+    total_doses: float = population * (target / 100) * 2
+    doses_left: float = total_doses - doses
+    days_left: float = doses_left / doses_per_day
+    return(int(round(days_left)))
 
 
 # TODO 3: Define future_date function
-def future_date(days_left) -> str:
-    """Produces the date the vaccinatino target will be met"""
+def future_date(days_left: int) -> str:
+    """Produces the date the vaccinatino target will be met."""
     today: datetime = datetime.today()
     days_left_time: timedelta = timedelta(days_left)
     date_complete: datetime = today + days_left_time
