86ee6322a829253c9e9373fa39965289219c5081
Andrew Best <acandrew@email.unc.edu>
2021-02-27 11:55:25 -0500

    added medium and hard game

--- projects/cyoa.py
+++ projects/cyoa.py
@@ -8,15 +8,25 @@ points: int = 0
 
 def main() -> None:
     """The entrypoint of the program."""
+    global points
     greet()
-    choice: str = input("easy, medium, hard, or end? ")
-    if choice == "easy":
-        easy()
-    else: 
-        if choice == "hard":
-            hard()
-        else:
-            end()
+    play_again: str = "yes"
+    while play_again == "yes":
+        choice: str = input("easy, medium, hard, or end? ")
+        if choice == "easy":
+            easy()
+            play_again = input(f"Score: {points}. Would you like to play again? (yes/no)")
+        else: 
+            if choice == "medium":
+                points = medium(points)
+                play_again = input(f"Score: {points}. Would you like to play again? (yes/no)")
+            else:
+                if choice == "hard":
+                    points = hard(points)
+                    play_again = input(f"Score: {points}. Would you like to play again? (yes/no)")
+                else:
+                    end()
+    end()
 
 
 def greet() -> None:
@@ -43,6 +53,44 @@ def easy() -> None:
         print(f"Round: {i} / 10. Score: {points}")
         i += 1
     end()
+
+
+def medium(points: int) -> int:
+    """Number guessing game where the user guesses number 1 to 5."""
+    print("In this game, the number to be guessed is between 1 and 5. Good luck!")
+    i: int = 1
+    while(i < 11):
+        num: str = str(randint(1,5))
+        guess: str = (input(f"Round {i}! Please enter guess: "))
+        if guess == num: 
+            points += 5
+            print("Correct!")
+        else: 
+            points += 1
+            print(f"Nope, the number was {num}")
+        print(f"Round: {i} / 10. Score: {points}")
+        i += 1
+    return points
+
+
+def hard(points: int) -> int:
+    """Number guessing game where the user guesses number 1 to 10."""
+    print("In this game, the number to be guessed is between 1 and 10. Good luck!")
+    i: int = 1
+    while(i < 11):
+        num: str = str(randint(1,10))
+        guess: str = (input(f"Round {i}! Please enter guess: "))
+        if guess == num: 
+            points
+            points += 10
+            print("Correct!")
+        else: 
+            points += 1
+            print(f"Nope, the number was {num}")
+        print(f"Round: {i} / 10. Score: {points}")
+        i += 1
+    return points
+
 
 def end() -> None:
     """Ends the game."""
