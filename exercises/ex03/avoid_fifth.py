0149444641782677087e45b6795cc0b4dcdbe0af
Andrew Best <acandrew@email.unc.edu>
2021-03-06 17:36:12 -0500

    completed avoid_fifth

--- exercises/ex03/avoid_fifth.py
+++ exercises/ex03/avoid_fifth.py
@@ -1,13 +1,29 @@
 """EX03 - avoid_fifth function."""
 
-__author__: str = "YOUR PID HERE"
+__author__: str = "730390102"
 
 
 def main() -> None:
     """Entrypoint of the program."""
+    print(avoid_fifth("Hello"))
+    print(avoid_fifth("The sentence we have here possesses E's galore!"))
     # Put print statements here to test your function
     # ex. print(avoid_fifth("hello there"))
 
+
+def avoid_fifth(text_input: str) -> str:
+    """Will remove any E's!"""
+    i = 0
+    text_list: list =[]
+    while i < len(text_input):
+        ordinal: int = ord(text_input[i])
+        if ordinal != 69 and ordinal != 101:
+            text_list.append(chr(ordinal))
+        i += 1
+    new_text: str = ""
+    for i in text_list:
+        new_text += i 
+    return(new_text)
 
 if __name__ == "__main__":
     main()
