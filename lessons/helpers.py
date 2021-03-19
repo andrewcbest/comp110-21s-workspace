098b33be7c8fa9ad89a3e1e71dd92865315a0eb6
Andrew Best <acandrew@email.unc.edu>
2021-03-19 16:10:39 -0400

    import lesson

---
+++ lessons/helpers.py
@@ -0,0 +1,20 @@
+"""Example functinos that can be imported elsewhere."""
+
+
+THE_ANSWER: int = 42
+
+def shout(message: str) -> None:
+    print(message.upper())
+
+
+def whisper(message: str) -> None: 
+    print(message.lower())
+
+
+print("helpers.py was evaluated")
+
+if __name__ == "__main__":
+    print("Helpers.py was run as a module")
+else: 
+    print(__name__)
+    print("helpers.py was imported as a module")
